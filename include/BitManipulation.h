118e79471d7375537e88d86439fc564f480d860f
Doug Park <dougparkarosa@gmail.com>
2022-09-25 19:12:03 -0700

    Initial checkin

---
+++ include/BitManipulation.h
@@ -0,0 +1,38 @@
+#ifndef BIT_MANIPULATION_H
+/// \brief Functions to manipulate bits.
+namespace bit_manipulation {
+
+/// Turn a specific bit on (set to 1)
+/// \tparam T Integral type holding bits to manipulate
+/// \param value location of bits to manipulate
+/// \param bit bit counting from right to turn on
+template <typename T> void on(T &value, unsigned int bit) {
+  value |= (1 << bit);
+}
+
+/// Turn a specific bit off (set to 0)
+/// \tparam T Integral type holding bits to manipulate
+/// \param value location of bits to manipulate
+/// \param bit bit counting from right to turn on
+template <typename T> void off(T &value, unsigned int bit) {
+  value &= ~(1 << bit);
+}
+
+/// Toggle a specific bit (set to 0 if 1 or 1 if 0)
+/// \tparam T Integral type holding bits to manipulate
+/// \param value location of bits to manipulate
+/// \param bit bit counting from right to turn on
+template <typename T> void toggle(T &value, unsigned int bit) {
+  value ^= (1 << bit);
+}
+
+/// Test if a particular bit is set (!=0)
+/// \tparam T Integral type holding bits to manipulate
+/// \param value location of bits to test
+/// \param bit bit counting from right to test
+/// \return true if bit is set false otherwise
+template <typename T> bool test(const T &value, unsigned int bit) {
+  return (value & (1 << bit)) != 0;
+}
+} // namespace bit
+#endif // BIT_MANIPULATION_H
---
+++ test/test_BitManipulation.cpp
@@ -0,0 +1,110 @@
+#include <unity.h>
+
+#include <Arduino.h>
+
+#include <BitManipulation.h>
+
+using namespace bit_manipulation;
+namespace {
+template <typename T> void testAllOn(T bits) {
+  for (unsigned int bit = 0; bit < sizeof(bits); ++bit) {
+    TEST_ASSERT(test(bits, bit));
+  }
+}
+
+template <typename T> void testAllOff(T bits) {
+  for (unsigned int bit = 0; bit < sizeof(bits); ++bit) {
+    TEST_ASSERT(!test(bits, bit));
+  }
+}
+} // namespace
+
+void test_bit_on() {
+
+  int bits = 0;
+
+  TEST_ASSERT(bits == 0);
+  on(bits, 0);
+  TEST_ASSERT(bits != 0);
+  TEST_ASSERT(bits == 1 << 0);
+
+  bits = 0;
+  for (unsigned int bit = 0; bit < sizeof(bits); ++bit) {
+    on(bits, bit);
+    TEST_ASSERT(bits > 0);
+    TEST_ASSERT(test(bits, bit));
+  }
+
+  testAllOn(bits);
+}
+
+void test_bit_off() {
+
+  int bits = 0;
+  for (unsigned int bit = 0; bit < sizeof(bits); ++bit) {
+    on(bits, bit);
+
+    for (unsigned int bit = 0; bit < sizeof(bits); ++bit) {
+      off(bits, bit);
+      TEST_ASSERT(!test(bits, bit));
+    }
+  }
+  testAllOff(bits);
+}
+
+void test_bit_toggle() {
+  using namespace bit_manipulation;
+  int bits = 0;
+  for (unsigned int bit = 0; bit < sizeof(bits); ++bit) {
+    toggle(bits, bit);
+    TEST_ASSERT(test(bits, bit));
+  }
+  testAllOn(bits);
+
+  for (unsigned int bit = 0; bit < sizeof(bits); ++bit) {
+    toggle(bits, bit);
+    TEST_ASSERT(!test(bits, bit));
+  }
+  testAllOff(bits);
+}
+
+void testSetSpeed() {
+  const long kNumIter = 10000L;
+
+  auto s1 = micros();
+  for (int i = 0; i < kNumIter; ++i) {
+    on(PORTB, PORTB5);
+  }
+  auto f1 = micros();
+  auto s2 = micros();
+  for (int i = 0; i < kNumIter; ++i) {
+    PORTB |= (1 << PORTB5);
+  }
+  auto f2 = micros();
+  TEST_ASSERT((f1 - s1) > 0);
+  TEST_ASSERT((f2 - s2) > 0);
+  TEST_ASSERT_INT_WITHIN(7, f1 - s1, f2 - s2);
+}
+
+void setup() {
+  // NOTE!!! Wait for >2 secs
+  // if board doesn't support software reset via Serial.DTR/RTS
+  delay(2000);
+
+  UNITY_BEGIN();
+  RUN_TEST(test_bit_on);
+  RUN_TEST(test_bit_off);
+  RUN_TEST(test_bit_toggle);
+  RUN_TEST(testSetSpeed);
+  UNITY_END();
+
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // Blink LED when tests are done
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
